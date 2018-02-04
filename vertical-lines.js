93783e0ed6476b0fce0af1792c52acf78be7f243
kingdish <31574274+kingdish@users.noreply.github.com>
2018-02-04 01:31:19 -0800

    Update vertical-lines.js

--- vertical-lines.js
+++ vertical-lines.js
@@ -1,9 +1,12 @@
 var n = 34;
 var startX = 0;
 var startY = 0;
+var endX = 0;
+var endY = 340;
 
 while (n > 0) {
-  line(startX, 0, startX, 340)
+  line(startX, startY, endX, endY);
   startX += 10;
+  endX += 10;
 	n--;
 }
