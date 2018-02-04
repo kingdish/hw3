4cb9d0322befdebf0cf5d0534db6920b71d574ac
kingdish <31574274+kingdish@users.noreply.github.com>
2018-02-04 02:32:56 -0800

    Create drawing-new-colors.js

---
+++ drawing-new-colors.js
@@ -0,0 +1,30 @@
+function setup() { 
+  createCanvas(400, 400);
+	noStroke();
+} 
+
+var diameter = 10;
+
+function draw() { 
+  if (mouseIsPressed) {
+    ellipse(mouseX, mouseY, diameter);
+  }
+}
+
+//Color palette from http://www.color-hex.com/color-palette/54431
+function keyPressed() {
+  print(key);
+  if (key == 'R') {
+    fill(242,169,137);
+  } else if (key == 'B') {
+    fill(165,195,246);
+  } else if (key == 'G') {
+    fill(151,219,98); 
+  } else if (key == 'Y') {
+    fill(251,211,88); 
+  } else if (key == 'O') {
+    fill(255,175,54); 
+  } else if (key == 'P') {
+    fill(201,159,255); 
+  }
+}
