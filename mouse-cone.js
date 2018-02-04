74d739a01949ed82b80ff63034aa7bcc5901c8ff
kingdish <31574274+kingdish@users.noreply.github.com>
2018-02-04 02:11:06 -0800

    Update mouse-cone.js

--- mouse-cone.js
+++ mouse-cone.js
@@ -1,1 +1,14 @@
-
+function draw() {
+  clear();
+  var n = 34;
+  var startX = mouseX;
+  var startY = mouseY;
+  var endX = 0;
+  var endY = 340;
+  
+  while (n > 0) {
+    line(startX, startY, endX, endY)
+    endX += 10;
+    n--;
+  }
+}
