ddc0806b71d9b3028f975e8c557ad5c249fa095c
kingdish <31574274+kingdish@users.noreply.github.com>
2018-02-04 01:45:11 -0800

    Update art-deco.js

--- art-deco.js
+++ art-deco.js
@@ -2,9 +2,19 @@ function singleImage(x, y, sideLength) {
   var n = 4;
 	while (n > 0) {
   	rect(x, y, sideLength, sideLength);
-    x += 7.5;
-    y += 7.5;
-    sideLength -= 15;
+    x += sideLength / n / 2;
+    y += sideLength / n / 2;
+    sideLength -= sideLength / n;
     n--;
   }
+}
+
+var n = 5;
+var startX = 0;
+var startY = 140;
+
+while (n > 0) {
+  singleImage(startX, startY, 60);
+  startX += 70;
+	n--;
 }
