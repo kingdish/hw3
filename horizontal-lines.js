595352409c9514a2fc64b9cd64e88335322d0c1e
kingdish <31574274+kingdish@users.noreply.github.com>
2018-02-04 01:30:43 -0800

    Update horizontal-lines.js

--- horizontal-lines.js
+++ horizontal-lines.js
@@ -1,9 +1,12 @@
 var n = 34;
 var startX = 0;
 var startY = 0;
+var endX = 340;
+var endY = 0;
 
 while (n > 0) {
-  line(0, startY, 340, startY)
+  line(startX, startY, endX, endY);
   startY += 10;
+  endY += 10;
 	n--;
 }
